fd5b03eacd80a1f7903a819a5e0f0a7b1ecbb5d9
CarlosRubio9827 <rubiogallegoc@gmail.com>
2022-01-02 23:17:28 -0500

    Fixed

--- src/components/ThemeComponent.jsx
+++ src/components/ThemeComponent.jsx
@@ -6,30 +6,35 @@ import { MdLightMode } from "react-icons/md";
 function ThemeComponent(){
 
 
-    const themes = [
-        {
-            name: 'Light',
-        },
-        {
-            name: 'Dark'
-        } 
-    ]
-
-    function example() {
-
-        document.getElementsByTagName("HTML")[0].setAttribute('data-theme', 'dark')
-    } 
-
-
+    const [checked, setChecked] = useState(localStorage.getItem("theme") === "dark" ? true : false);
     
+
+            // ? document.getElementsByTagName("HTML")[0].setAttribute('data-theme', 'dark')
+            // : document.getElementsByTagName("HTML")[0].setAttribute('data-theme', '')
+    
+    useEffect(() => {
+        document.getElementsByTagName("HTML")[0].setAttribute("data-theme", localStorage.getItem("theme"));        
+    }, [checked]);
+   
+    const toggleThemeChange = () => {
+        if (checked === false) {
+          localStorage.setItem("theme", "dark");
+          setChecked(true);
+        } else {
+          localStorage.setItem("theme", "light");
+          setChecked(false);
+        }
+      };
 
     return (
         <div className="themeComponent">
             <div className="themeComponent-title">
                 <p>DevFinder</p>
             </div>
-            <div className="themeComponent-change" onClick={example}>
-                <input type="checkbox" id="switch" /><label htmlFor="switch">Toggle</label>
+            <div className="themeComponent-change" >
+                <input type="checkbox" defaultChecked={checked} onChange={()=>{
+                toggleThemeChange()
+            }}  id="switch" /><label htmlFor="switch">Toggle</label>
             </div>
         </div>
     ) 
